591b6174349dc99d75dfdc85f1b675952715d00d
Hakeem <hakeem.abdul@shopee.com>
2024-04-24 18:38:31 +0800

    add python upload api

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import base64
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/process_document/")
+async def process_document(file: UploadFile = File(...)):
+    contents = await file.read()
+    file_base64 = base64.b64encode(contents).decode("utf-8")
+    return {"filename": file.filename, "content": file_base64}
+
