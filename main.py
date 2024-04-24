014d12d90174085a10c8d2b6a86d5d1c7681131a
Sihao Chen <chensihao96@gmail.com>
2024-04-24 19:41:53 +0800

    refactor and add context

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, UploadFile, File
 import base64
-from model import generate
+from model import Model
 
 app = FastAPI()
-
+model = Model()
 
 @app.post("/process_document/")
 async def process_document(file: UploadFile = File(...)):
     contents = await file.read()
     file_base64 = base64.b64encode(contents).decode("utf-8")
-    return generate(file_base64)
+    return model.generate(file_base64)
 
--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import base64
 import json
+import time
+
 import vertexai
 from vertexai.generative_models import GenerativeModel, Part, FinishReason
 import vertexai.preview.generative_models as generative_models
@@ -18,23 +20,48 @@ safety_settings = {
 }
 
 
-def generate(document):
-    vertexai.init(project="polar-equinox-420601", location="us-central1")
-    model = GenerativeModel("gemini-1.5-pro-preview-0409")
+def retry_on_error(max_retries=2, delay=1):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            retries = 0
+            while retries < max_retries:
+                try:
+                    result = func(*args, **kwargs)
+                    return result
+                except Exception as e:
+                    print(f"Error occurred: {e}")
+                    retries += 1
+                    print(f"Retrying ({retries}/{max_retries}) after {delay} seconds...")
+                    time.sleep(delay)
+            raise Exception(f"Failed after {max_retries} retries.")
 
-    document1 = Part.from_data(
-    mime_type="application/pdf",
-    data=base64.b64decode(document))
-    text1 = """You are a carbon analyst accounting for carbon emissions from aluminium production. You need to 
-    extract the materials from the invoice and the purchase agreement, and convert the units into SI unit.  Classify 
-    the materials into the different scope and sub category using the context information below. The final output 
-    should be in a table format. Add minimal justification in the table, as well as the source of the material using 
-    its file name, eg. MOCK INVOICE.pdf. Output the result in JSON format."""
-    responses = model.generate_content(
-        [text1, document1],
-        generation_config=generation_config,
-        safety_settings=safety_settings,
-        stream=True,
-    )
-    for response in responses:
-        print(response.text, end="")
+        return wrapper
+
+    return decorator
+
+
+class Model():
+    def __init__(self):
+        vertexai.init(project="polar-equinox-420601", location="us-central1")
+        self.model = GenerativeModel("gemini-1.5-pro-preview-0409")
+
+        with open('prompt/prompt.txt', 'r') as file:
+            # Read the contents of the file
+            self.prompt = file.read()
+
+        with open('prompt/context.txt', 'r') as file:
+            # Read the contents of the file
+            self.context = file.read()
+
+    @retry_on_error(max_retries=2, delay=1)
+    def generate(self, document):
+        document1 = Part.from_data(
+            mime_type="application/pdf",
+            data=base64.b64decode(document))
+        responses = self.model.generate_content(
+            [self.prompt, document1, self.context],
+            generation_config=generation_config,
+            safety_settings=safety_settings,
+            stream=False,
+        )
+        return json.loads(responses.text.replace("json", "").replace("\n", "").replace("```", "").replace("```", ""))
